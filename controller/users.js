9d0442ac63aa9312e6727d4ba7fc8146c2d6c203
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-04-15 10:50:50 +0530

    Helpers file removed

--- controller/users.js
+++ controller/users.js
@@ -3,9 +3,7 @@ module.exports=function(Product,local,fs,async){
     return{
         SetRouting:function(router){
             router.get("/",this.homePage);
-            router.get("/submit",this.GetSubmitpage);
             router.get("/product-compare/:name",this.getCompare);
-            router.post("/submit",local.Upload.single("productImage"),this.SubmitProduct);
         },
         homePage:function(req,res){
             async.parallel([
@@ -23,26 +21,6 @@ module.exports=function(Product,local,fs,async){
                 var res1=results[0];
                 var res2=results[1];
                 res.render("home",{data:res1,data1:res2});
-            })
-        },
-        GetSubmitpage:function(req,res){
-            return res.render("submit");
-        },
-        SubmitProduct:function(req,res){
-            var img = fs.readFileSync(req.file.path);
-            var encode_image = img.toString('base64');
-            var finalImg = {
-                contentType: req.file.mimetype,
-                image:  Buffer.from(encode_image, 'base64')
-            };
-            var newProduct=new Product();
-            newProduct.Name=req.body.Name;
-            newProduct.Description=req.body.Description;
-            newProduct.ProductImage.data=finalImg.image;
-            newProduct.ProductImage.contentType=finalImg.contentType;
-            newProduct.Price=req.body.Price;
-            newProduct.save(function(err,msg){
-                res.redirect("/");
             })
         },
         getCompare:function(req,res){
