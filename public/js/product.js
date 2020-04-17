cdd004aafe4673acdc75afe203e5493ae5fd4378
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-04-17 10:33:47 +0530

    Function of Compare improved

--- public/js/product.js
+++ public/js/product.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
     $(".add-card").on("click",function(e){
         e.preventDefault();
         var Id=$(this).siblings(".Product-id").val();
-        product_Id.push(Id);
+        if(product_Id.length==0){
+            product_Id.push(Id);
+        }
+        else if(product_Id.length==1 && product_Id[0]!=Id){
+            product_Id.push(Id);
+        }
         if(product_Id.length==2){
             var Id=product_Id[0]+"+"+product_Id[1];
             product_Id=[];
