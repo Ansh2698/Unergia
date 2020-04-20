b8da9b84e32a67b3ada85cb0dee5509ea4b786f0
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-04-20 12:56:06 +0530

    navbar added

--- public/js/main.js
+++ public/js/main.js
@@ -29,10 +29,27 @@ $(window).on('load', function() {
 	});
 
 
-	
+	/*------------------
+		ScrollBar
+	--------------------*/
+	$(".cart-table-warp, .product-thumbs").niceScroll({
+		cursorborder:"",
+		cursorcolor:"#afafaf",
+		boxzoom:false
+	});
 
 
-	
+	/*------------------
+		Category menu
+	--------------------*/
+	$('.category-menu > li').hover( function(e) {
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('.category-menu').mouseleave( function(e) {
+		$('.category-menu li').removeClass('active');
+		e.preventDefault();
+	});
 
 
 	/*------------------
@@ -101,4 +118,108 @@ $(window).on('load', function() {
 			}
 		}
 	});
+
+
+	/*------------------
+		Popular Services
+	--------------------*/
+	$('.popular-services-slider').owlCarousel({
+		loop: true,
+		dots: false,
+		margin : 40,
+		autoplay: true,
+		nav:true,
+		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			768 : {
+				items: 2,
+			},
+			991: {
+				items: 3
+			}
+		}
+	});
+
+
+	/*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').removeClass('active');
+		var $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+
+
+	/*-------------------
+		Range Slider
+	--------------------- */
+	var rangeSlider = $(".price-range"),
+		minamount = $("#minamount"),
+		maxamount = $("#maxamount"),
+		minPrice = rangeSlider.data('min'),
+		maxPrice = rangeSlider.data('max');
+	rangeSlider.slider({
+		range: true,
+		min: minPrice,
+		max: maxPrice,
+		values: [minPrice, maxPrice],
+		slide: function (event, ui) {
+			minamount.val('$' + ui.values[0]);
+			maxamount.val('$' + ui.values[1]);
+		}
+	});
+	minamount.val('$' + rangeSlider.slider("values", 0));
+	maxamount.val('$' + rangeSlider.slider("values", 1));
+
+
+	/*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+	proQty.prepend('<span class="dec qtybtn">-</span>');
+	proQty.append('<span class="inc qtybtn">+</span>');
+	proQty.on('click', '.qtybtn', function () {
+		var $button = $(this);
+		var oldValue = $button.parent().find('input').val();
+		if ($button.hasClass('inc')) {
+			var newVal = parseFloat(oldValue) + 1;
+		} else {
+			// Don't allow decrementing below zero
+			if (oldValue > 0) {
+				var newVal = parseFloat(oldValue) - 1;
+			} else {
+				newVal = 0;
+			}
+		}
+		$button.parent().find('input').val(newVal);
+	});
+
+
+
+	/*------------------
+		Single Product
+	--------------------*/
+	$('.product-thumbs-track > .pt').on('click', function(){
+		$('.product-thumbs-track .pt').removeClass('active');
+		$(this).addClass('active');
+		var imgurl = $(this).data('imgbigurl');
+		var bigImg = $('.product-big-img').attr('src');
+		if(imgurl != bigImg) {
+			$('.product-big-img').attr({src: imgurl});
+			$('.zoomImg').attr({src: imgurl});
+		}
+	});
+
+
+	$('.product-pic-zoom').zoom();
+
+
+
 })(jQuery);
